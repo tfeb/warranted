143c22aa345979b7c44bfd09b1b2ae5e01c71922
Tim Bradshaw <tfb@cley.com>
2019-05-03 11:54:08 +0100

    A bunch of tests for wct.rkt
    Also abstract what a valid wct node name is, in preparation for
    wildcards.

--- wct.rkt
+++ wct.rkt
@@ -13,24 +13,47 @@
            (->* () ((listof (and/c path? absolute-path?))) valid-wct?)))
          (struct-out exn:fail:bad-wct-spec))
 
+(module+ test
+  (require rackunit))
+
 (define (valid-wct? thing)
   ;; A tree is a list of nodes, possibly a null list
   (and (list? thing)
        (for/and ([node thing])
          (valid-wct-node? node))))
 
+(define (valid-wct-node-name? name)
+  ;; node names are strings (for now)
+  (string? name))
+
 (define (valid-wct-node? node)
   ;; a node is either null, which matches
   (or (null? node)
       ;;  or a cons of (string . tree)
       (and (cons? node)
-           (string? (car node))
+           (valid-wct-node-name? (car node))
            (valid-wct? (cdr node)))))
 
+(module+ test
+  (for ([n (in-list '("foo" ""))])
+    (check-true (valid-wct-node-name? n)))
+  (check-false (valid-wct-node-name? 'x))
+
+  (check-true (valid-wct-node? '("x")))
+  (check-false (valid-wct-node? '("x" "y")))
+
+  (check-true (valid-wct? '(("foo"))))
+  (check-true (valid-wct? '()))
+  (check-false (valid-wct? '("foo" "bar")))
+
+  (let ([node '("cat" ("foo") ("bar") ("fish" ("x") ("y")))])
+    (check-true (valid-wct-node? node))
+    (check-true (valid-wct? (list node)))))
+
 (define (valid-file-entry? entry)
-  ;; a fle entry ie either a list of strings or a WCT node
+  ;; a file entry ie either a list of strings or a WCT node
   (or (and (list? entry)
-           (andmap string? entry))
+           (andmap valid-wct-node-name? entry))
       (valid-wct-node? entry)))
 
 (define (valid-file-entries? entries)
@@ -51,16 +74,39 @@
   ;; turn a list of file entries into a WCT
   (map file-entry->wct-node entries))
 
+(module+ test
+  (check-true (valid-file-entry? '("cat" "/etc/motd")))
+  (check-true (valid-file-entries? '(("echo" "hi")
+                                     ("echo")
+                                     ("cp" "foo" "bar"))))
+  ;; See below for checks that these are equivalent to the simpler translations
+  (check-equal? (file-entry->wct-node '("cat" "foo"))
+                '("cat" ("foo" ())))
+  (check-equal? (file-entries->wct '(("echo" "hi")
+                                    ("cp" "foo" "bar")))
+               '(("echo" ("hi" ()))
+                 ("cp" ("foo" ("bar" ()))))))
+
 (define (valid-slist? thing)
   ;; a valid string list
   (and (list? thing)
        (andmap string? thing)))
 
+(module+ test
+  (for ([s (in-list '(("foo" "bar")
+                      ("foo")))])
+    (check-true (valid-slist? s)))
+  (for ([s (in-list '(a ("b" . "c")
+                        (* "1")))])
+    (check-false (valid-slist? s))))
+
+;;; Some abstraction so the matcher can think in terms of trees.
+;;; Note that the children of a node are explicitly a list of nodes still,
+;;; and a WCT is a list of nodes.
+;;;
 (define wct-node-null? null?)
 (define wct-node-key car)
 (define wct-node-children cdr)
-(define append-wcts append)
-(define null-wct '())
 
 (define (slist-matches-wct? slist wct)
   (if (null? slist)
@@ -71,6 +117,20 @@
           (and (not (wct-node-null? node))
                (string=? slfirst (wct-node-key node))
                (slist-matches-wct? slrest (wct-node-children node)))))))
+
+(module+ test
+  (let ([wct/small '(("cat" ("foo")))]
+        [wct/bigger '(("cat" ("foo" ())
+                             ("bar")))]
+        [cf '("cat" "foo")]
+        [cb '("cat" "bar")]
+        [cff '("cat" "foo" "foo")])
+    (check-true (slist-matches-wct? cf wct/small))
+    (check-true (slist-matches-wct? cf wct/bigger))
+    (check-false (slist-matches-wct? cb wct/small))
+    (check-true (slist-matches-wct? cb wct/bigger))
+    (check-false (slist-matches-wct? cff wct/small))
+    (check-false (slist-matches-wct? cff wct/bigger))))
 
 
 ;;;; Reading WCTs from files
@@ -91,15 +151,14 @@
       (build-path home "etc" wcf)))))
 
 (define (read-wct (files (warranted-commands-files)))
-  (define (read-one-wct file)
-    (if (file-exists? file)
-        (let ([spec (call-with-default-reading-parameterization
-                     (thunk (call-with-input-file file read)))])
-          (unless (valid-file-entries? spec)
-            (raise (make-exn:fail:bad-wct-spec
-                    "bad WCT spec"
-                    (current-continuation-marks)
-                    spec file)))
-          (file-entries->wct spec))
-        null-wct))
-  (apply append-wcts (map read-one-wct files)))
+  (for/fold ([wct '()]) ([file files])
+    (append wct (if (file-exists? file)
+                    (let ([spec (call-with-default-reading-parameterization
+                                 (thunk (call-with-input-file file read)))])
+                      (unless (valid-file-entries? spec)
+                        (raise (make-exn:fail:bad-wct-spec
+                                "bad WCT spec"
+                                (current-continuation-marks)
+                                spec file)))
+                      (file-entries->wct spec))
+                    '()))))
